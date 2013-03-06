ea1016072de61cfe149aa97732c9b4ed5dcffe19
Amit Dhakad <amitdha@thoughtworks.com>
2013-03-06 16:14:32 +0000

    added heroku toolbelt

--- puppet/manifests/dev/vagrant.pp
+++ puppet/manifests/dev/vagrant.pp
@@ -6,6 +6,9 @@ path => "/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin"
 
 class { 'vagrant' : }
 
+exec { 'heroku toolbelt' :
+command => 'wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh' }
+
 include postgresql::server
 
 postgresql::db_instance{
